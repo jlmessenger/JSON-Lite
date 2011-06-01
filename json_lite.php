69f1a48dbba3a74f30cbb756d4219a6959744593
Jeremy Messenger <jlmessengertech+github@gmail.com>
2011-06-01 16:45:32 -0400

    Pick specific license

--- json_lite.php
+++ json_lite.php
@@ -8,7 +8,7 @@
  * @version   1.0
  * @author    Jeremy Messenger <jlmessengertech+github@gmail.com>
  * @copyright 2011 Jeremy Messenger
- * @license   GPL
+ * @license   LGPL <http://www.gnu.org/licenses/lgpl.html>
  * @link      http://jlmessenger.com
  */
 
